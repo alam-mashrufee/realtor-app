851a9353188a5ac68afc3edd24cad74d57fc217a
alam-mashrufee <mashrufee@gmail.com>
2022-06-15 17:08:46 +0900

    signup Logic

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
+import * as jwt from 'jsonwebtoken';
 
 interface SignupParams {
   name: string;
@@ -11,7 +14,7 @@ interface SignupParams {
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  async signup({ email }: SignupParams) {
+  async signup({ email, name, phone, password }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: {
         email,
@@ -22,6 +25,27 @@ export class AuthService {
       throw new ConflictException();
     }
 
-    return [];
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashPassword,
+        user_type: UserType.BUYER,
+      },
+    });
+
+    const token = await jwt.sign(
+      {
+        name,
+        id: user.id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      { expiresIn: 3600000 },
+    );
+
+    return token;
   }
 }
