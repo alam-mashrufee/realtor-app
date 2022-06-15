a8097f1ed8efaceadb3598e65f231f714c40b107
alam-mashrufee <mashrufee@gmail.com>
2022-06-15 12:55:10 +0900

    signup logic

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+interface SignupParams {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+}
 
 @Injectable()
 export class AuthService {
-  signup() {
+  constructor(private readonly prismaService: PrismaService) {}
+  async signup({ email }: SignupParams) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      throw new ConflictException();
+    }
+
     return [];
   }
 }
